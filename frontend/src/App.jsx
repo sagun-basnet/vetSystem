8ab5b86bb622943e41f4f1992765ac86478ba432
sagun basnet <basnetsagun8@gmail.com>
2025-01-07 20:17:39 +0545

    home page fixed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -95,7 +95,7 @@ const App = () => {
 
     return (
         <div className="flex justify-center min-h-screen">
-            <div className="App font-heading w-full max-w-[343px]  md:max-w-[704px] lg:max-w-[1240px] xl:max-w-[100%]">
+            <div className="App font-heading w-full max-w-[343px]  md:max-w-[704px] lg:max-w-[1240px] ">
                 <RouterProvider router={router} />
             </div>
         </div>
