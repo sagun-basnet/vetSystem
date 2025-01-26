23e6667cfd5bf59d9e2b363eb1eaf3c541e98c28
sagun basnet <basnetsagun8@gmail.com>
2025-01-26 09:35:21 +0545

    OTP verification complete

--- api/controller/appointment.js
+++ api/controller/appointment.js
@@ -1,14 +1,14 @@
 import { db } from "../db/db.js";
 export const bookAppointment = (req, res) => {
-  const { date, service, user_id } = req.body;
-  const q = "select * from appointment where date = ?";
+  const { date, service, user_id, doctor_id } = req.body;
+  const q = "select * from appointment where date = ? and doctor_id = ?";
   db.query(q, [date], (err, results) => {
     if (err) return res.status(500).json(err);
     if (results.length)
       return res.status(409).json("Appointment already exists.");
     const q =
-      "insert into appointment (date, service, user_id) values(?, ?, ?)";
-    db.query(q, [date, service, user_id], (err, result) => {
+      "insert into appointment (date, service, user_id, doctor_id) values(?, ?, ?, ?)";
+    db.query(q, [date, service, user_id, doctor_id], (err, result) => {
       if (err) return res.status(500).json(err);
       res
         .status(201)
--- api/routes/appointmentRoute.js
+++ api/routes/appointmentRoute.js
@@ -1,10 +1,11 @@
 import express from "express";
 import { isAuth } from "../middleware/isAuth.js";
-import { bookAppointment, getAppointment } from "../controller/appointment.js";
+import { bookAppointment, getAppointment, cancelAppointment } from "../controller/appointment.js";
 
 const route = express.Router();
 
 route.post("/book-appointment", isAuth, bookAppointment);
 route.get("/get-appointment", isAuth, getAppointment);
+route.post("/cancel-appointment/:id", isAuth, cancelAppointment);
 
 export default route;
