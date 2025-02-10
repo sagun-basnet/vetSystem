03b74fd89f6a64740e3e4bbb3e7ef8c7a478a366
sujan-pakhrin <sujannntmg@gmail.com>
2025-02-10 22:01:45 +0545

    changes in appointment

--- api/controller/appointment.js
+++ api/controller/appointment.js
@@ -1,14 +1,16 @@
 import { db } from "../db/db.js";
 export const bookAppointment = (req, res) => {
   const { date, service, user_id, doctor_id } = req.body;
+ const doctorId=parseInt(doctor_id);
+ console.log(doctorId);
   const q = "select * from appointment where date = ? and doctor_id = ?";
-  db.query(q, [date], (err, results) => {
+  db.query(q, [date,doctorId], (err, results) => {
     if (err) return res.status(500).json(err);
     if (results.length)
       return res.status(409).json("Appointment already exists.");
     const q =
       "insert into appointment (date, service, user_id, doctor_id) values(?, ?, ?, ?)";
-    db.query(q, [date, service, user_id, doctor_id], (err, result) => {
+    db.query(q, [date, service, user_id, doctorId], (err, result) => {
       if (err) return res.status(500).json(err);
       res
         .status(201)
