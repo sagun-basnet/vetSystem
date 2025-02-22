44e1b66f650f9884985c28dd21427462229f8e3c
sagun basnet <basnetsagun8@gmail.com>
2025-02-22 11:29:08 +0545

    appointment by doctor

--- api/routes/appointmentRoute.js
+++ api/routes/appointmentRoute.js
@@ -1,12 +1,19 @@
 import express from "express";
 import { isAuth } from "../middleware/isAuth.js";
-import { bookAppointment, getAppointment, cancelAppointment, getAppointmentByUser } from "../controller/appointment.js";
+import {
+  bookAppointment,
+  getAppointment,
+  cancelAppointment,
+  getAppointmentByUser,
+  getAppointmentByDoctor,
+} from "../controller/appointment.js";
 
 const route = express.Router();
 
 route.post("/book-appointment", isAuth, bookAppointment);
 route.get("/get-appointment", isAuth, getAppointment);
 route.get("/getAppointmentByUser/:id", isAuth, getAppointmentByUser);
+route.get("/getAppointmentByDoctor/:id", isAuth, getAppointmentByDoctor);
 route.post("/cancel-appointment/:id", isAuth, cancelAppointment);
 
 export default route;
